0e09a4e233ba51ca829bca1a289bf6248254282b
BadassOverlord <dboy740@gmail.com>
2017-09-14 22:30:11 -0700

    completed 12 assignment

---
+++ 12.py
@@ -0,0 +1,33 @@
+
+
+story = '''
+Hold it {}!
+{}. But! {} only if ye be {} of valor!
+For {} is guarded by a {} so {}, so {}, that no {} yet has {} with it... and {}!
+'''
+
+def main():
+    command = input("Enter a command (e.g. Sleep): ")
+    plural_noun = input("Enter a plural noun (e.g. trees): ")
+    animal = input("Enter an animal (e.g. cow): ")
+    location = input("Enter a location (e.g. Surrey OR the playground: ")
+    singular_noun = input("Enter a singular noun (e.g. tree): ")
+    person_name = input("Enter the name of a person.")
+    adjectives = [input("Enter an adjective (e.g. big): "), input("Enter another adjective: ")]
+    adverb = input("Enter an adverb (e.g. slowly): ")
+    past_participle = [input("Enter a past participle (e.g. played): "), input("Enter another past participle: ")]
+
+    mad_lib = story.format(person_name,
+                           command,
+                           command,
+                           plural_noun,
+                           location,
+                           animal,
+                           adjectives[0],
+                           adjectives[1],
+                           adverb,
+                           past_participle[0],
+                           past_participle[1])
+    print(mad_lib)
+
+main()
